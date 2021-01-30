3330362f0188e4fb0e75c7b0b0d37b7406adf6bf
Natalio Rabasco Navarro <62501046+Natalio-R@users.noreply.github.com>
2021-01-30 23:38:24 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { ChatEngine } from "react-chat-engine";
+
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+import "./App.css";
+
+const projectID = "34fbe7cf-e60b-4ae3-a484-6d7b262656c7";
+
+const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+        ).play()
+      }
+    />
+  );
+};
+
+export default App;
